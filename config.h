dea837164c42676cfcd36391a3c68be7b1ab7e01
Andrew Pobrica <pirit66@gmail.com>
2022-05-21 22:45:46 -0400

    Massive restructure of code, struct is passed to kernels now, added some documentation

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 // The amount to pad the energy grid in each direction.
 #define PADDING 1
 // Granularity of energy calculations
-#define GRIDSPACING .1
-#define THREADSPERBLOCK 128
+#define GRIDSPACING .02
+#define THREADSPERBLOCK 32
 
 
--- d_main.cu
+++ d_main.cu
@@ -6,7 +6,7 @@
 #include "molecule.h"
 #include "d_main.h"
 
-__global__ void d_discombulateKernel(float * energyGrid, const float *atoms, dim3 grid, float gridspacing,
+__global__ void d_discombulateKernel(float * energyGrid, const atom *atoms, dim3 grid, float gridspacing,
                                       int numatoms);
 
 __global__ void d_discombulateKernelConst(float * energyGrid, dim3 grid, float gridSpacing,
@@ -22,11 +22,13 @@ static __global__ void emptyKernel();
     atoms: An array of all atoms of the molecule and their positions. 
     numAtoms: The number of atoms in the molecule.
 */
-__constant__ float constAtoms[1024];
-
-int d_discombobulate(float * energyGrid, float * atoms, int dimX, int dimY, int dimZ, float gridSpacing,  int numAtoms, int which){
+__constant__ atom constAtoms[256];
+int d_discombobulate(float * energyGrid, atom * atoms, int dimX, int dimY, int dimZ, float gridSpacing,  int numAtoms, int which){
     cudaEvent_t start_gpu, stop_gpu;
-    float gpuMsecTime = -1;
+    float gpuMsecTime = -1;;
+
+
+
 
     CHECK(cudaEventCreate(&start_gpu));
     CHECK(cudaEventCreate(&stop_gpu));
@@ -43,28 +45,32 @@ int d_discombobulate(float * energyGrid, float * atoms, int dimX, int dimY, int 
     float * d_energyGrid;
     CHECK(cudaMalloc((void**)&d_energyGrid, gridSize));
 
-    float * d_atoms;
-    CHECK(cudaMalloc((void**)&d_atoms, numAtoms * 4 * sizeof(int)));
-    CHECK(cudaMemcpy(d_atoms, atoms, numAtoms * 4 * sizeof(int), cudaMemcpyHostToDevice));
+
 
     dim3 grid(dimX, dimY, dimZ);
 
     if (which == 0) {
+        atom * d_atoms;
+        CHECK(cudaMalloc((void**)&d_atoms, numAtoms * sizeof(atom)));
+        CHECK(cudaMemcpy(d_atoms, atoms, numAtoms * sizeof(atom), cudaMemcpyHostToDevice));
+
         dim3 blockDim(THREADSPERBLOCK, 1, 1);
-        dim3 gridDim(ceil((1.0 * dimZ) / THREADSPERBLOCK), 1, 1);
+        dim3 gridDim(ceil((1.0 * dimX) / THREADSPERBLOCK), 1, 1);
         d_discombulateKernel<<<gridDim, blockDim>>>(d_energyGrid, d_atoms, grid, gridSpacing, numAtoms);
+
+
+        CHECK(cudaFree(d_atoms));
     }
     if (which == 1) {
         CHECK(cudaMemcpyToSymbol(constAtoms, atoms, sizeof(float) * numAtoms * 4));
 
         dim3 blockDim(THREADSPERBLOCK, 1, 1);
-        dim3 gridDim(ceil((1.0 * dimZ) / THREADSPERBLOCK), 1, 1);
+        dim3 gridDim(ceil((1.0 * dimX) / THREADSPERBLOCK), 1, 1);
         d_discombulateKernelConst<<<gridDim, blockDim>>>(d_energyGrid, grid, gridSpacing, numAtoms);
     }
 
     CHECK(cudaMemcpy(energyGrid, d_energyGrid, gridSize, cudaMemcpyDeviceToHost));
 
-    CHECK(cudaFree(d_atoms));
     CHECK(cudaFree(d_energyGrid));
 
     CHECK(cudaEventRecord(stop_gpu));
@@ -79,58 +85,81 @@ int d_discombobulate(float * energyGrid, float * atoms, int dimX, int dimY, int 
     A kernel that calculates the electrostatic potential and stores it 
     in a float array
 
-    energyGrid: The float array associated with the molecule 
+    This performed about 7x better than cpu before switching to atom struct
+
+    energyGrid: A float array of the sample points in and around the molecule
+    atoms: an array of structs that have all the necessary information about each atom, see molecule.h
+    grid: a dim3 struct containing the dimensions of energyGrid
+    gridSpacing: the space between grid points along each axis
+    numAtoms: number of atoms in atoms array
 */
-__global__ void d_discombulateKernel(float * energyGrid, const float *atoms, dim3 grid, float gridSpacing,
+__global__ void d_discombulateKernel(float * energyGrid, const atom *atoms, dim3 grid, float gridSpacing,
                                      int numAtoms) {
 
 
     int i, j, n;
-    if (blockDim.x * blockIdx.x + threadIdx.x < grid.z) {
-        float z = gridSpacing * (threadIdx.x + blockIdx.x * blockDim.x);
-        int atomArrDim = numAtoms * 4;
-        for (j = 0; j < grid.y; j++) {
-            float y = gridSpacing * (float) j;
-            for (i = 0; i < grid.x; i++) {
-                float x = gridSpacing * (float) i;
+    if (blockDim.x * blockIdx.x + threadIdx.x < grid.x) {
+        float x = gridSpacing * (threadIdx.x + blockIdx.x * blockDim.x);
+
+            for (i = 0; i < grid.z; i++) {
+                float z = gridSpacing * (float) i;
+                for (j = 0; j < grid.y; j++) {
+                    float y = gridSpacing * (float) j;
                 float energy = 0.0f;
-                for (n = 0; n < atomArrDim; n += 4) {
-                    float dx = x - atoms[n];
-                    float dy = y - atoms[n + 1];
-                    float dz = z - atoms[n + 2];
-                    energy += atoms[n + 3] / sqrt(dx * dx + dy * dy + dz * dz);
+                for (n = 0; n < numAtoms; n ++) {
+                    float dx = x - atoms[n].x;
+                    float dy = y - atoms[n].y;
+                    float dz = z - atoms[n].z;
+                    energy += atoms[n].charge / sqrtf(dx * dx + dy * dy + dz * dz);
                 }
-                energyGrid[grid.x * grid.y * (blockIdx.x * blockDim.x + threadIdx.x) + grid.x * j + i] = energy;
+                    __syncthreads();
+                    energyGrid[grid.x * grid.y * i + grid.x * j + (blockIdx.x * blockDim.x + threadIdx.x)] = energy;
+
             }
         }
     }
 }
+
+/*
+ * d_discombobulateKernel
+ * A kernel that calculates the electrostatic potential and stores it in a float array,
+    this version uses constant memory instead of passing in an atoms array
+ *
+ * energyGrid: A float array of the sample points in and around the molecule
+ * grid: a dim3 struct containing the dimensions of energyGrid
+ * gridSpacing: the space between grid points along each axis
+ *   numAtoms: number of atoms in constAtoms
+ */
 
 __global__ void d_discombulateKernelConst(float * energyGrid, dim3 grid, float gridSpacing,
                                      int numAtoms) {
 
 
     int i, j, n;
-    if (blockDim.x * blockIdx.x + threadIdx.x < grid.z) {
-        float z = gridSpacing * (threadIdx.x + blockIdx.x * blockDim.x);
-        int atomArrDim = numAtoms * 4;
-        for (j = 0; j < grid.y; j++) {
-            float y = gridSpacing * (float) j;
-            for (i = 0; i < grid.x; i++) {
-                float x = gridSpacing * (float) i;
+    if (blockDim.x * blockIdx.x + threadIdx.x < grid.x) {
+        float x = gridSpacing * (threadIdx.x + blockIdx.x * blockDim.x);
+
+            for (i = 0; i < grid.z; i++) {
+                float z = gridSpacing * (float) i;
+                for (j = 0; j < grid.y; j++) {
+                    float y = gridSpacing * (float) j;
                 float energy = 0.0f;
-                for (n = 0; n < atomArrDim; n += 4) {
-                    float dx = x - constAtoms[n];
-                    float dy = y - constAtoms[n + 1];
-                    float dz = z - constAtoms[n + 2];
-                    energy += constAtoms[n + 3] / sqrt(dx * dx + dy * dy + dz * dz);
+                for (n = 0; n < numAtoms; n ++) {
+                    float dx = x - constAtoms[n].x;
+                    float dy = y - constAtoms[n].y;
+                    float dz = z - constAtoms[n].z;
+                    float charge = constAtoms[n].charge;
+                    energy += charge / sqrtf(dx * dx + dy * dy + dz * dz);
                 }
-                energyGrid[grid.x * grid.y * (blockIdx.x * blockDim.x + threadIdx.x) + grid.x * j + i] = energy;
+                    __syncthreads();
+                    energyGrid[grid.x * grid.y * i + grid.x * j + (blockIdx.x * blockDim.x + threadIdx.x)] = energy;
+
             }
         }
     }
 }
 
+// an empty kernel to improve timing?
 __global__ void emptyKernel()
 {
 }
